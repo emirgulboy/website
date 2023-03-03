a5207ead4994b52349e4c0ac7bba0e289c712aa4
emirgulboy@gmail.com <emirgulboy@gmail.com>
2023-03-03 13:01:06 +0300

    Pİc holder done Create galery

--- my-app/src/pages/home.jsx
+++ my-app/src/pages/home.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
-import ImageHoverSlow from '../components/imageholder';
-import Pic1 from '../assets/images/thumbnail.webp';
-import ass from '../assets/images/logo_final.png'
+import ImageHover from '../components/imageholder';
+import Pic1 from "../assets/images/thumbnail.webp";
 
 console.log(Pic1)
 function Home() {
   return (
     <><div>
       <h1> Home </h1>
-      <image src={ass}></image>
-      <ImageHoverSlow
-      defaultImgPath={Pic1} />
-      <img
-        src="https://bobbyhadz.com/images/blog/react-prevent-multiple-button-clicks/thumbnail.webp"
-        alt="car"
-      />
+      <ImageHover imgSrc={Pic1} />
     </div>
     </>
               
